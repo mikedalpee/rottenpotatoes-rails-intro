71c0f89de13f5d379f005b004485d972938f838a
Mike Dalpee <mikedalpee@gmail.com>
2017-09-23 17:41:41 +0000

    rails hw part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,7 +13,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    sort=params[:sort]
+    sort=params[:sort] || session[:sort]
     case sort
     when 'title'
       ordering = {:title => :asc}
@@ -23,9 +23,15 @@ class MoviesController < ApplicationController
       @date_header = 'hilite'
     end
     @all_ratings = Movie.all_ratings
-    @selected_ratings = params[:ratings] || {}
+    @selected_ratings = params[:ratings] || session[:ratings] || {}
     if @selected_ratings == {}
       @selected_ratings = Hash[@all_ratings.map{|rating| [rating,1]}]
+    end
+    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+      session[:sort] = sort
+      session[:ratings] = @selected_ratings
+      flash.keep
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
     end
     @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
   end
